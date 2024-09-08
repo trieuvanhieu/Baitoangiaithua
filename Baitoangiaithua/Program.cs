58a85976c274056b4343ffa50f2569ba30be3a3c
Your Name <you@example.com>
2024-09-08 15:19:25 +0700

    Add project files.

---
+++ Baitoangiaithua/Program.cs
@@ -0,0 +1,17 @@
+﻿//Triệu Văn Hiếu
+//Bài toán giai thừa
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitoangiaithua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
