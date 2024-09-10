690e440cde993470b15e0f0650ea34d1a1b2320f
Your Name <you@example.com>
2024-09-10 23:27:19 +0700

    ud

--- ConsoleApp4/Giaithua1.cs
+++ ConsoleApp4/Giaithua1.cs
@@ -1,18 +1,18 @@
-﻿// Namespace của thư viện
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp4
 {
-    // Class tính toán giai thừa
     public class Giaithua1
     {
         // Phương thức tính giai thừa
         public long TinhGiaiThua(int n)
         {
             if (n < 0)
-            {
-                throw new ArgumentException("Số phải là số nguyên dương.");
-            }
+                throw new ArgumentException("So phai la so nguyen duong.");
 
             long giaiThua = 1;
             for (int i = 1; i <= n; i++)
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,19 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            // Tạo đối tượng của class Giaithua từ DLL
+            Giaithua1 giaithua = new Giaithua1();
+
+            // Nhập số nguyên dương từ người dùng
+            Console.WriteLine("Nhập một số nguyên dương:");
+            int n = int.Parse(Console.ReadLine());
+
+            // Gọi hàm tính giai thừa và in kết quả ra màn hình
+            long result = giaithua.TinhGiaiThua(n);
+            Console.WriteLine($"Giai thừa của {n} là: {result}");
+
+            // Cho người dùng bấm phím trước khi thoát
+            Console.ReadKey();
         }
     }
 }
